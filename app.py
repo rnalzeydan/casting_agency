d705570c97adc1c6424590e40950fabf32c3d268
Raghad <rnalzydan@hotmail.com>
2020-07-04 11:09:30 +0300

    create app

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request, abort, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  CORS(app)
+
+  return app
+
+APP = create_app()
+
+@APP.route('/')
+def index():
+    return jsonify({
+            'message': 'Hello'
+        })
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ setup.sh
@@ -0,0 +1,4 @@
+export AUTH0_DOMAIN='fsndraghad1.us.auth0.com'
+export API_AUDIENCE='casting_agency '
+export ALGORITHMS=RS256
+export  CLIENT_ID='wyL1PQTdS6darrSRn2WuYAmvVGM39j9W'
