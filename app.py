da5af49f0b7b2efd01ef506642335048a4d3edb4
Raghad <rnalzydan@hotmail.com>
2020-07-04 12:18:56 +0300

    add delete and post methods

--- app.py
+++ app.py
@@ -33,6 +33,111 @@ def create_app(test_config=None):
             'actors': formated_actors
         })
 
+    @app.route('/movies')
+    @requires_auth('get:movies')
+    def retrieve_movies(jwt):
+
+        movies = Movie.query.all()
+        formated_movies = [movie.format() for movie in movies]
+
+        if len(movies) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'movies': formated_movies
+        })
+
+    '''
+    DELETE Method
+    '''
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actor(jwt, id):
+
+        actor = Actor.query.filter(Actor.id == id).one_or_none()
+
+        if actor is None:
+            abort(404)
+
+        try:
+            actor.delete()
+
+            return jsonify({
+                'success': True,
+                'actor': actor.format()
+            })
+
+        except Exception:
+            abort(422)
+
+    @app.route('/movies/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:movies')
+    def delete_movie(jwt, id):
+
+        movie = Movie.query.filter(Movie.id == id).one_or_none()
+
+        if movie is None:
+            abort(404)
+
+        try:
+            movie.delete()
+
+            return jsonify({
+                'success': True,
+                'movie': movie.format()
+            })
+
+        except Exception:
+            abort(422)
+
+    '''
+    POST Method
+    '''
+
+    @app.route('/actors', methods=['POST'])
+    @requires_auth('post:actors')
+    def add_actor(jwt):
+
+        body = request.get_json()
+
+        new_name = body.get('name', None)
+        new_age = body.get('age', None)
+        new_gender = body.get('gender', None)
+
+        try:
+            actor = Actor(name=new_name, age=new_age, gender=new_gender)
+            actor.insert()
+
+            return jsonify({
+                'success': True,
+                'actor': actor.format()
+            })
+
+        except Exception:
+            abort(422)
+
+    @app.route('/movies', methods=['POST'])
+    @requires_auth('post:movies')
+    def add_movie(jwt):
+
+        body = request.get_json()
+
+        new_title = body.get('title', None)
+        new_release_date = body.get('release_date', None)
+
+        try:
+            movie = Movie(title=new_title, release_date=new_release_date)
+            movie.insert()
+
+            return jsonify({
+                'success': True,
+                'movie': movie.format()
+            })
+
+        except Exception:
+            abort(422)
+
     return app
 
 
